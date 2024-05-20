d6e282592aa24babf0682f4eedd2a1d973574405
Andres Minota <andresnotami@gmail.com>
2024-05-19 23:08:34 -0500

    feat: :sparkles: Add and work in component list and cards

--- src/app/breeds/components/card/card.component.ts
+++ src/app/breeds/components/card/card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Breed } from '../../interfaces/breed.interface';
 
 @Component({
-  selector: 'app-card',
+  selector: 'breeds-breed-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input()
+  public breed!: Breed;
 
+  ngOnInit(): void {
+    if (!this.breed) {
+      throw new Error('Breed is required');
+    }
+
+    console.log(this.breed);
+  }
 }
--- src/app/breeds/pages/list-page/list-page.component.ts
+++ src/app/breeds/pages/list-page/list-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Breed } from '../../interfaces/breed.interface';
+import { BreedsService } from '../../services/breeds.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styleUrl: './list-page.component.css'
+  styleUrl: './list-page.component.css',
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  public breeds: Breed[] = [];
+  public page: number = 1;
 
+  constructor(private breedsService: BreedsService) {}
+
+  ngOnInit(): void {
+    this.breedsService
+      .getBreeds()
+      .subscribe((breeds) => (this.breeds = breeds));
+  }
+
+  /**
+   * @description Loads more breeds and appends them to the existing list.
+   */
+  loadMore(): void {
+    this.page++;
+    this.breedsService
+      .getBreeds(this.page)
+      .subscribe(
+        (breeds: Breed[]) => (this.breeds = [...this.breeds, ...breeds])
+      );
+  }
 }
