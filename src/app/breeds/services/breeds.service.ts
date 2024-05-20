476e6b08a20426048cbca4bd2460d37bcd98c755
Andres Minota <andresnotami@gmail.com>
2024-05-19 23:07:10 -0500

    feat: :sparkles: Add config and services for cat-breeds

---
+++ src/app/breeds/services/breeds.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { enviroments } from 'src/enviroments/enviroments';
+import { Breed } from '../interfaces/breed.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BreedsService {
+  private baseUrl = enviroments.baseUrl;
+  private apiKey = enviroments.apiKey;
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * @description Retrieves a list of cat breeds.
+   *
+   * @param page The page number to retrieve (default: 1).
+   * @param pageSize The number of breeds per page (default: 10).
+   * @returns An Observable that emits an array of Breed objects.
+   */
+  getBreeds(page: number = 1, pageSize: number = 10): Observable<Breed[]> {
+    return this.http.get<Breed[]>(
+      `${this.baseUrl}/v1/breeds?api_key=${this.apiKey}&limit=${pageSize}&page=${page}`
+    );
+  }
+}
---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const enviroments = {
+  baseUrl: 'https://api.thecatapi.com',
+  apiKey:
+    'live_bdTOqF1QpljGQlAlYxOHkKzeN4BDOzSC6Z5sFtFwMuKZthflK4OZ7a7hMZXmM04R',
+};
